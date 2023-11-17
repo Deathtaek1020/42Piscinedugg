dab0ff227895d01ce901dcd37d69dc4220f16146
Deathtaek1020 <66733557+Deathtaek1020@users.noreply.github.com>
2023-11-18 00:38:27 +0300

    C05

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deathtaek <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 00:35:53 by deathtaek         #+#    #+#             */
+/*   Updated: 2023/11/18 00:37:14 by deathtaek        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_is_prime(int nb)
+{
+	int	div;
+
+	div = 2;
+	if (nb < 2)
+		return (0);
+	while (div <= (nb / div))
+	{
+		if (nb % div == 0)
+			return (0);
+		div++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
+}
