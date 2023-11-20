7198a66ad05d43c7671ddd34f7879e96fe7944ce
Deathtaek1020 <66733557+Deathtaek1020@users.noreply.github.com>
2023-11-20 21:23:41 +0300

    Add files via upload

---
+++ Poins -3/C02 update 1/ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deathtaek <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 19:36:02 by deathtaek         #+#    #+#             */
+/*   Updated: 2023/11/20 19:44:08 by deathtaek        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	l;
+
+	i = 0;
+	l = 0;
+	while (src[l] != '\0')
+		l++;
+	if (size != 0)
+	{
+		while ((src[i] != '\0') && (i < (size - 1)))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (l);
+}
